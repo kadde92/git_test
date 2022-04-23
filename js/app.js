56997dc0a8c42cc74540212e84729bd7a4c05894
kadde92 <kadir.wafin@gmail.com>
2022-04-23 13:48:05 +0300

    random stuff

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("kadir on paras")
+let multiply = (numberOne, numberTwo) => numberOne * numberTwo;
+console.log(multiply(2,8));
+
+
+let capitalize = string => `${string[0].toUpperCase()}${string.slice(1).toLowerCase()}`;
+
+console.log(capitalize('jaromir'))
+
+let lastLetter = last => last.slice(-1);
+
+console.log(lastLetter('kadir'))
+
